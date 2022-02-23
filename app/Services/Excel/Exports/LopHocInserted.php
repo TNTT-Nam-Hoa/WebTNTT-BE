38ccd6adf52cdd863ea09efcc7eeb3151432772a
longhm <longhm@fireapps.vn>
2022-02-23 13:30:37 +0700

    Upgrade Excel - Add importer-exporter

---
+++ app/Services/Excel/Exports/LopHocInserted.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Services\Excel\Exports;
+
+use Carbon\Carbon;
+use Illuminate\Contracts\Support\Responsable;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Excel;
+
+class LopHocInserted implements FromArray, Responsable, WithTitle, WithHeadings, ShouldAutoSize, WithEvents
+{
+    use Exportable;
+
+    private $fileName = 'TaoMoi_LopHoc_';
+    private $writerType = Excel::XLSX;
+    private $data = null;
+
+    public function __construct($data)
+    {
+        $this->data     = $data;
+        $this->fileName .= Carbon::now()->format('d-m-Y_h-i-s').'.xlsx';
+    }
+
+    public function title(): string
+    {
+        return 'Danh Sach';
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $event->sheet->freezePane('A2');
+            },
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Mã Lớp',
+            'Tên',
+            'Vị Trí Học',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function array(): array
+    {
+        $results = [];
+        foreach ($this->data as $item) {
+            $results[] = [
+                $item['id'],
+                $item['ten'],
+                $item['vi_tri_hoc'],
+            ];
+        }
+        return $results;
+    }
+}
---
+++ app/Services/Excel/Imports/TaiKhoanImport.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Services\Excel\Imports;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+use App\Exceptions\ExcelInvalidFormat;
+use App\KhoaHoc;
+use App\LopHoc;
+
+class TaiKhoanImport implements ToCollection, WithHeadingRow, WithMultipleSheets
+{
+    protected $data = [];
+
+    /**
+     * @return array
+     */
+    public function sheets(): array
+    {
+        return [
+            0 => $this,
+        ];
+    }
+
+    /**
+     * $row = [
+     *     0  => "Ngành",
+     *     1  => "Cấp",
+     *     2  => "Đội",
+     *     3  => "Loại Tài Khoản",
+     *     4  => "Tên Thánh",
+     *     5  => "Họ và Tên",
+     *     6  => "Giới Tính",
+     *     7  => "Ngày Sinh",
+     *     8  => "Ngày Rửa Tội",
+     *     9  => "Ngày Ruớc Lễ",
+     *     10 => "Ngày Thêm Sức",
+     *     11 => "Điện Thoại",
+     *     12 => "Địa Chỉ",
+     *     13 => null,
+     * ];
+     *
+     * @param  Collection  $rows
+     * @return array
+     * @throws ExcelInvalidFormat
+     */
+    public function collection(Collection $rows)
+    {
+        $khoaId    = KhoaHoc::hienTai()->id;
+        $lopHocArr = LopHoc::where('khoa_hoc_id', $khoaId)->get();
+
+        $rows = $rows->whereNotNull('ngay_sinh')->whereNotNull('ho_va_ten')
+            ->map(function ($c) {
+                $c['ngay_sinh']     = mapDate($c['ngay_sinh']);
+                $c['ngay_rua_toi']  = $c['ngay_rua_toi'] ? mapDate($c['ngay_rua_toi']) : null;
+                $c['ngay_ruoc_le']  = $c['ngay_ruoc_le'] ? mapDate($c['ngay_ruoc_le']) : null;
+                $c['ngay_them_suc'] = $c['ngay_them_suc'] ? mapDate($c['ngay_them_suc']) : null;
+
+                return $c;
+            });
+
+        $tmpRule = [
+            'ngay_sinh'     => 'required|date_format:Y-m-d',
+            'ngay_rua_toi'  => 'nullable|date_format:Y-m-d',
+            'ngay_ruoc_le'  => 'nullable|date_format:Y-m-d',
+            'ngay_them_suc' => 'nullable|date_format:Y-m-d',
+        ];
+
+        foreach ($rows as $index => $row) {
+            $validator = Validator::make($row->toArray(), $tmpRule);
+            if ($validator->fails()) {
+                throw  new ExcelInvalidFormat([
+                    'error' => $validator->errors(),
+                    'item'  => $row,
+                ]);
+            }
+
+            $lop = $lopHocArr->filter(function ($lh) use ($row) {
+                return $lh->nganh == $row['nganh'] && $lh->cap == $row['cap'] && $lh->doi == $row['doi'];
+            })->first();
+
+            if ($lop) {
+                $row = array_merge($row->toArray(), [
+                    'lop_hoc_id'  => $lop->id,
+                    'lop_hoc_ten' => $lop->taoTen(),
+                ]);
+            }
+
+            $this->data[] = $row;
+        }
+
+        return $this->data;
+    }
+
+    /**
+     * @return array
+     */
+    public function getResult()
+    {
+        return $this->data;
+    }
+}
